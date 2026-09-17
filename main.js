b0970377856b20c67e416d79110b899987d5e31b
fuadcabbarov97 <fuadcabbarov42@gmail.com>
2026-09-17 22:22:10 +0400

    yaddas oyunu

---
+++ main.js
@@ -0,0 +1,21 @@
+let numbers = [4, 7, 2, 9, 1];
+let answer = numbers.join("");
+alert("Yadda saxlayın: " + answer);
+let uduzdu = true;
+for(let i = 0; i < 3; i++) {
+  let user = prompt("ƏDƏDLƏRİ EYNİ ARDICILLIQLA DAXİL EDİN:");
+    if (user === numbers.join("")) {
+        console.log("AFERIN");
+        let uduzdu=false
+        break
+    }
+
+    
+if (uduzdu) {
+    console.log("UDUZDUNUZ!");
+
+}
+
+
+}
+
