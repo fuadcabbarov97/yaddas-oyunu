f026cbf719e0061bdfb1e8273b73d6079cc84a44
fuadcabbarov97 <fuadcabbarov42@gmail.com>
2026-09-18 22:23:23 +0400

    SADE EDEDLER

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
+// YADDAS OYUNU:
+
 let numbers = [4, 7, 2, 9, 1];
 let answer = numbers.join("");
 alert("Yadda saxlayın: " + answer);
 let uduzdu = true;
-for(let i = 0; i < 3; i++) {
-  let user = prompt("ƏDƏDLƏRİ EYNİ ARDICILLIQLA DAXİL EDİN:");
+for (let i = 0; i < 3; i++) {
+    let user = prompt("ƏDƏDLƏRİ EYNİ ARDICILLIQLA DAXİL EDİN:");
     if (user === numbers.join("")) {
-        console.log("AFERIN");
-        let uduzdu=false
+        console.log("AFERIN!");        
+        console.log("VİDEOYA BAXDIĞINIZ ÜCÜN TƏŞƏKKÜRLƏR!:))))))");
+        let uduzdu = false
         break
+
     }
 
-    
-if (uduzdu) {
-    console.log("UDUZDUNUZ!");
 
-}
+    if (uduzdu) {
+        console.log("UDUZDUNUZ!");
+
+    }
 
 
 }
